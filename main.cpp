6e3a498d54064ec182adc663c4fe8bd584582a4a
Xavi <mrjavoman@hotmail.com>
2016-02-07 15:57:52 -0600

    adding gumbo-parser reference to project in main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
 
-int main()
+#include "gumbo.h"
+
+int main(int argc, char** argv)
 {
 
-   std::cout << "Welcome to the paper search command line utility" << std::endl;
+   std::cout << "Welcome to the paper search command line utility\n"; 
+
+   if(argc != 2) {
+      std::cout << "Usage: paperSearch <html filename>\n";
+      exit(EXIT_FAILURE);
+   }  
+
+   const char* filename = argv[1];
+
+   std::ifstream in(filename, std::ios::in | std::ios::binary);
+   if (!in) {
+     std::cout << "File " << filename << " not found!\n";
+     exit(EXIT_FAILURE);
+   }
+
+   std::string contents;
+   in.seekg(0, std::ios::end);
+   contents.resize(in.tellg());
+   in.seekg(0, std::ios::beg);
+   in.read(&contents[0], contents.size());
+   in.close();
+
+   GumboOutput* output = gumbo_parse(contents.c_str());
+   /* Code for extracting paper info */
+   // std::cout << cleantext(output->root) << std::endl;
+   
+   /* --------- */
+   gumbo_destroy_output(&kGumboDefaultOptions, output);
+   
+   std::cout << "End Program\n";
 
    return 0;
 }

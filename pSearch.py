db9753fc8966cdbf948cca73359497cf7dec0060
Xavi <mrjavoman@hotmail.com>
2016-04-24 16:26:02 -0500

    refactored code to use functions

--- pSearch.py
+++ pSearch.py
@@ -1,90 +1,98 @@
-#try:
-import urllib.request as urllib
-#except ImportError:
-#    import urllib2
-from multiprocessing.dummy import Pool as ThreadPool
-from html.parser import HTMLParser
 import pdb
-
-from lxml import html
-import requests
 import sys
-
-#from oauth2client.client import flow_from_clientsecrets
-
-#flow = flow_from_clientsecrets('./client_secrets.json',
-#                               scope='https://www.googleapis.com/auth/calendar',
-#                               redirect_uri='http://example.com/auth_return')
-
-#auth_uri = flow.step1_get_authorize_url()
-
-#auth_response = requests.get(auth_uri)
+import json
 import pprint
-
+from multiprocessing.dummy import Pool as ThreadPool
 from apiclient.discovery import build
 
-namesFile = open(sys.argv[1])
-keywords_input = open(sys.argv[2])
-keywords = []
+#--------------------------------------------------------
+# Function for querying results
+#--------------------------------------------------------
+def queryGoogleApi(name, service) :
 
-for word in keywords_input :
-    keywords.append(word)
-
-
-# Open file for writing the results
-filename = "Results.txt"
-target = open(filename, 'w')
-
-paperList = []
-
-for name in namesFile:
-    print(name)
-    target.write(name.strip())
-    target.write("\nResearch Papers:")
-    service = build("customsearch", "v1", developerKey="AIzaSyCbyFJHL8l2p-DdCGnd92rAhVF9SQzdjoI")
+    paperList = []
 
     res = service.cse().list(
       q=name,
-      cx='018078494062619471299:7nycalkac1w',
+      cx=config_data["searchApi"]["customsearchID"],
     ).execute()
 
-    #cx='017576662512468239146:omuauf_lfve',
     #pdb.set_trace()
     pprint.pprint(res['items'][0]['title'])
-
+    paperList.append({"researcher": res['items'][0]['title'],"paper": "NotFound", "keyword": "NotFound"})
     # for each paper find search for each keyword
-    #pdb.set_trace()
     for paper in res['items'][0]['pagemap']['scholarlyarticle'] :
-
         for keyword in keywords :
-            #print(keyword)
-        #    pdb.set_trace()
             if keyword.strip().lower() in paper['name'].lower() :
                 pprint.pprint(paper)
-                paperTuple = [paper, keyword]
+                paperTuple = {"researcher": res['items'][0]['title'],"paper": paper, "keyword": keyword.strip()}
                 paperList.append(paperTuple)
-                target.write(paper['name'])
-                target.write("\n")
-                #pdb.set_trace()
                 break
 
-    target.write("\n")
+    paperList.append({"researcher": "","paper": "", "keyword": "separator"})
 
-target.close()
+    return paperList
 
-pdb.set_trace()
+#--------------------------------------------------------
+# Function for outputing results
+#--------------------------------------------------------
+def outputResults(output_filename, paperList) :
+
+    target = open(output_filename, 'w')
+    currResearcher = ""
+
+    for entry in paperList :
+        if entry["paper"] == "" :
+            target.write("---------------------------------------------------------\n")
+        else :
+            if(currResearcher != entry["researcher"]) :
+                currResearcher = entry["researcher"]
+                target.write(entry["researcher"] + "\n")
+                target.write("---------------------------------------------------------\n")
+
+            if(entry['paper'] != "NotFound" ) :
+                target.write("Article: " + entry['paper']['name'] + "\n")
+                target.write("Keyword: " + entry['keyword'] + "\n\n")
+
+
+    target.close()
+
+#--------------------------------------------------------
+# Main
+#--------------------------------------------------------
+# Get configuration file
+with open('config.json') as data_file:
+    config_data = json.load(data_file)
+
+# Open input files
+namesFile = open(sys.argv[1])
+keywords_input = open(sys.argv[2])
+
+# Variables
+keywords = []
+paperList = []
+output_filename = "Results.txt"
+
+# Get keywords from input file into a resuable array
+for word in keywords_input :
+    keywords.append(word)
+
+# Open google api service
+service = build(config_data["searchApi"]["type"], config_data["searchApi"]["version"], developerKey=config_data["searchApi"]["developerKey"])
+
+# Process each researcher name and call google api
+for name in namesFile:
+    paperList.extend(queryGoogleApi(name, service))
+
+# Output results
+outputResults(output_filename, paperList)
 
 #page = requests.get('http://dblp.uni-trier.de/pers/hd/l/Liu:Tongping')
 #tree = html.fromstring(page.content)
 # Get paper names
 #paperTitles = tree.xpath('//span[@class="title"]/text()')
 
-#import webbrowser
-#import os
-
 #webbrowser.open('file://' + os.path.realpath("results.html"));
-
-#pdb.set_trace()
 
 
 # Make the Pool of workers
